2daf28f47e49cf011cb031b64058dbe56eb33e6a
sailesh <vermasailesh271@gmail.com>
2024-05-11 14:24:41 +0530

    added input handler and onpress to button

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,9 +1,33 @@
-import { Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
-export function PrimaryButton({ children }) {
+export function PrimaryButton({ children, onPress }) {
     return (
-        <View>
-            <Text>{children}</Text>
+        <View style={styles.buttonOuterContainer}>
+            <Pressable
+                style={styles.containerButtonStyle}
+                onPress={onPress}
+                android_ripple={{ color: "#640233" }}
+            >
+                <Text style={styles.buttonTextStyle}>{children}</Text>
+            </Pressable>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    containerButtonStyle: {
+        elevation: 4,
+        paddingVertical: 10,
+        backgroundColor: "#891a52",
+    },
+    buttonOuterContainer: {
+        borderRadius: 28,
+        overflow: 'hidden',
+        margin: 4,
+    },
+    buttonTextStyle: {
+        color: "white",
+        fontSize: 18,
+        textAlign: "center",
+    },
+});
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,18 +1,60 @@
-import { StyleSheet, TextInput, View } from "react-native";
+import { Alert, StyleSheet, TextInput, View } from "react-native";
 import { PrimaryButton } from '../components/PrimaryButton';
+import { useState } from "react";
 
 export function StartGameScreen() {
+    const [enteredText, setEnteredText] = useState("");
+
+    function onResetHandler() {
+        setEnteredText('')
+    }
+
+    function onChangeTextHandler(text) {
+        setEnteredText(text)
+        console.log(text);
+    }
+
+    function onConfirmHandler() {
+        const enteredNumber = parseInt(enteredText)
+        if (isNaN(enteredNumber) || enteredNumber < 1 || enteredNumber > 99) {
+            Alert.alert('Invalid Number', 'Please enter a valid number in range 1 to 99', [{
+                onPress: onResetHandler,
+                style: "destructive",
+                text: "Okay",
+            }])
+
+            return;
+        }
+        console.log('WAS PRESSEd')
+    }
+
+
     return (
         <View style={styles.inputContainer}>
-            <TextInput style={styles.inputTextStyle} maxLength={2} />
-            <PrimaryButton> First</PrimaryButton>
-            <PrimaryButton> Second</PrimaryButton>
+            <TextInput
+                style={styles.inputTextStyle}
+                keyboardType="phone-pad"
+                autoCapitalize="none"
+                maxLength={2}
+                autoCorrect={false}
+                value={enteredText}
+                onChangeText={onChangeTextHandler}
+            />
+            <View style={styles.buttonsHolderContainer}>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton onPress={onResetHandler} > Reset</PrimaryButton>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton onPress={onConfirmHandler}> Confirm</PrimaryButton>
+                </View>
+            </View>
         </View >
     );
 }
 
 const styles = StyleSheet.create({
     inputContainer: {
+        alignItems: "center",
         marginTop: 200,
         padding: 10,
         marginHorizontal: 20,
@@ -22,7 +64,6 @@ const styles = StyleSheet.create({
     },
     inputTextStyle: {
         height: 50,
-        alignSelf: "center",
         width: 50,
         textAlign: "center",
         borderBottomColor: "#ddb52f",
@@ -30,5 +71,12 @@ const styles = StyleSheet.create({
         borderBottomWidth: 2,
         marginVertical: 8,
         color: "#ddb52f",
+        fontWeight: "600",
+    },
+    buttonsHolderContainer: {
+        flexDirection: 'row',
+    },
+    buttonContainer: {
+        flex: 1
     }
 });
