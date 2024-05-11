f197dab1aa6c730c275394ce2466d2f64886d1c3
sailesh <vermasailesh271@gmail.com>
2024-05-11 14:24:15 +0530

    added bg image to materail screem

--- App.js
+++ App.js
@@ -1,18 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import { StartGameScreen } from './screens/StartGameScreen';
 
 export default function App() {
   return (
-    <StartGameScreen />
+    <LinearGradient colors={['#82063C', '#ddbf2f']} style={styles.materialApp}>
+      <ImageBackground
+        style={styles.materialApp}
+        source={require('./assets/images/bg.jpg')}
+        resizeMode='cover'
+        imageStyle={
+          styles.imageContainer
+        }
+      >
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
+  materialApp: {
+    flex: 1,
+
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imageContainer: {
+    opacity: 0.15
+  }
 });
